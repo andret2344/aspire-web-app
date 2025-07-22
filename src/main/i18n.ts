b8518940282d24e12bd559ecc503168de26c55fe
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-07-22 16:59:24 +0200

    SupportedLngs added for i18next

--- src/main/i18n.ts
+++ src/main/i18n.ts
@@ -8,6 +8,7 @@ i18n.use(Backend)
 	.use(initReactI18next)
 	.init({
 		fallbackLng: 'en',
+		supportedLngs: ['pl-PL', 'en-GB'],
 		debug: false, //process.env.NODE_ENV === 'development',
 		interpolation: {
 			escapeValue: false
