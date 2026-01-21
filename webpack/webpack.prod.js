490c332e5b77c58eda2b2b94c2844fbd59477570
Andret2344 <andret2344@gmail.com>
2026-01-21 11:06:23 +0100

    EX: Webpack 3

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,13 @@
-require('webpack');
-const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 module.exports = {
 	mode: 'production',
 	plugins: [
-		new Dotenv({path: '.env'})
-	]
+		new webpack.DefinePlugin({
+			'process.env': JSON.stringify({
+				NODE_ENV: 'production',
+				REACT_APP_API_TOKEN: process.env.REACT_APP_API_TOKEN ?? '',
+			}),
+		}),
+	],
 };
