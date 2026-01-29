eb6080ac62d1edb7dd50056074b2c924f80b0626
Andret2344 <andret2344@gmail.com>
2026-01-29 11:17:40 +0100

    EX: Review follow-up

--- src/tests/__mocks__/MockReactMarkdown.tsx
+++ src/tests/__mocks__/MockReactMarkdown.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 type AnchorType = React.ComponentType<React.AnchorHTMLAttributes<HTMLAnchorElement>>;
 
-type MarkdownProps = {
-	children?: React.ReactNode;
+interface MarkdownProps {
 	components?: {
 		a?: AnchorType;
 		[key: string]: AnchorType | undefined;
 	};
-};
+}
 
-export default (props: MarkdownProps): React.ReactElement => {
+export default (props: React.PropsWithChildren<MarkdownProps>): React.ReactElement => {
 	const content = props.children as string;
 	const linkMatch = content.match(/\[([^\]]+)]\(([^)]+)\)/);
 
